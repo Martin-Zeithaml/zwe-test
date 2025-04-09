f67d5e3902e968bff9fca7312fcaeab537b2bda1
Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>
2025-04-09 14:51:03 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+EXAMPLE_ZOWE='example-zowe.yaml'
+
 if  [ -z "${ZWET_ZOWE_RUNTIME_DIRECTORY}" ]; then
     . ./zwe-test.conf
     if  [ -z "${ZWET_ZOWE_RUNTIME_DIRECTORY}" ]; then
         echo "Environment variable 'ZWET_ZOWE_RUNTIME_DIRECTORY' not set."
         echo "Update 'zwe-test.conf' or set environment variable ZWET_ZOWE_RUNTIME_DIRECTORY=/path/to/zowe/runtime"
         exit 1
+    fi
+    if [ ! -d "${ZWET_ZOWE_RUNTIME_DIRECTORY}" ]; then
+        echo "'${ZWET_ZOWE_RUNTIME_DIRECTORY}' is not a valid directory"
+        exit 1
+    fi
+    if [ ! -f "${ZWET_ZOWE_RUNTIME_DIRECTORY}/${EXAMPLE_ZOWE}" ]; then
+        echo "Warning: file '${EXAMPLE_ZOWE}' not found in '${ZWET_ZOWE_RUNTIME_DIRECTORY}'"
     fi
 fi
 
@@ -17,24 +26,10 @@ cd "${currentDirectory}"
 
 export ZWET_ZOWE_RUNTIME_DIRECTORY="${zoweRuntimeAbsolutePath}"
 
-# Check/read symbolic link or create new one
+# Create symbolic link
 SYM_LINK='./lib/.zwe-test-zowe.runtimeDirectory'
-if [ -e "${SYM_LINK}" ]; then
-    if [ -L "${SYM_LINK}" ]; then
-        if [ "$(readlink ${SYM_LINK})" != "${zoweRuntimeAbsolutePath}" ]; then
-            echo "'${SYM_LINK}' is a soft link but does not point to '${zoweRuntimeAbsolutePath}'. Please move it out of the way and re-run this script."
-            exit 2
-        else
-            echo "'${SYM_LINK}' exists and points to ${zoweRuntimeAbsolutePath}"
-        fi
-    else
-        echo "${SYM_LINK} exists but is not a symbolic link. Remove it and re-run this script."
-        exit 3
-    fi
-else
-    echo "Linking '${zoweRuntimeAbsolutePath}' to '${SYM_LINK}'"
-    ln -s "${zoweRuntimeAbsolutePath}" "${SYM_LINK}"
-fi
+echo "Linking '${zoweRuntimeAbsolutePath}' to '${SYM_LINK}'"
+ln -fs "${zoweRuntimeAbsolutePath}" "${SYM_LINK}"
 
 ZWET_CONFIGMGR="${zoweRuntimeAbsolutePath}/bin/utils/configmgr"
 unset FILE
